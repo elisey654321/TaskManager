72261457f5ec77b53754630b0215318722a17834
elisey654321 <elisey654321@mail.ru>
2023-05-29 16:21:31 +0300

    +

--- src/main/java/ru/iedev/taskmanager/model/leetcode/hasCycle/Solution.java
+++ src/main/java/ru/iedev/taskmanager/model/leetcode/hasCycle/Solution.java
@@ -1,4 +1,20 @@
 package ru.iedev.taskmanager.model.leetcode.hasCycle;
 
+import ru.iedev.taskmanager.model.leetcode.reverseList.ListNode;
+
+import java.util.HashSet;
+
 public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        HashSet<ListNode> listNodes = new HashSet<>();
+        while (head != null){
+            if (listNodes.contains(head)){
+                return true;
+            }else listNodes.add(head);
+            head = head.next;
+        }
+        return false;
+
+    }
 }
